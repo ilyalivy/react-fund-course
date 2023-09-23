ab25e6101de6d42448566c9894c64561ef8d6414
ilyalivy <128998085+ilyalivy@users.noreply.github.com>
2023-09-23 15:10:55 +0300

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,64 @@
-import React, { useState, useRef } from 'react';
-import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
+import React, { useState, useEffect } from 'react';
 import './styles/App.css';
-import PostItem from './components/PostItem';
+import axios from 'axios';
 import PostList from './components/PostList';
+import PostForm from './components/PostForm';
+import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+import { usePosts } from './hooks/usePosts';
+import PostService from './API/PostService';
+import Loader from './components/UI/Loader/Loader';
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript 1', body: 'Description'},
-        {id: 2, title: 'Javascript 2', body: 'Description'},
-        {id: 3, title: 'Javascript 3', body: 'Description'}
-    ])
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    
-    const addNewPost = (e) => {
-        e.preventDefault()
-        const newPost = {
-            id: Date.now(),
-            title,
-            body
-        }
-       setPosts([...posts, newPost])
-       setTitle('')
-       setBody('')
-       
-    }
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
-    return (
-        <div className="App">
-            <form>
-                {/* Управляемый компонент */}
-                <MyInput
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                    type="text" 
-                    placeholder='Post name'
-                />
-                {/* Неуправляемый/Неконтролируемый компонент */}
-                <MyInput 
-                    value={body}
-                    onChange={e => setBody(e.target.value)}
-                    type="text" 
-                    placeholder='Post description'
-                />
-                <MyButton onClick={addNewPost}>Create post</MyButton>
-            </form>
-            <PostList posts={posts} title='JS Posts'/>
-        </div>
-    );
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+
+  async function fetchPosts() {
+    setIsPostsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setIsPostsLoading(false);
+  }
+
+  // Get post from child component
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  return (
+    <div className="App">
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Create post
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+      <hr style={{ margin: '15px 0' }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {isPostsLoading ? (
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader /></div>
+      ) : ( 
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="JS Posts"
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import PostItem from './PostItem'
+import React from 'react';
+import PostItem from './PostItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-const PostList = ({posts, title}) => {
+const PostList = ({ posts, title, remove }) => {
+  if (!posts.length) {
+    return <h1 style={{ textAlign: 'center' }}>Posts not found!</h1>;
+  }
+
   return (
     <div>
-        <h1 style={{textAlign: 'center'}}>
-            {title}
-        </h1>
-        {posts.map((post, index) =>
-            <PostItem number={index + 1} post={post} key={post.id}/>
-        )}
+      <h1 style={{ textAlign: 'center' }}>{title}</h1>
+      <TransitionGroup>
+        {posts.map((post, index) => (
+          <CSSTransition key={post.id} j timeout={500} classNames="post">
+            <PostItem remove={remove} number={index + 1} post={post} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
-  )
-}
+  );
+};
 
-export default PostList
+export default PostList;
